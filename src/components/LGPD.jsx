6b20d833360520f184c3d051fef3b47baca17264
flytec07 <flytec07@gmail.com>
2021-10-19 19:26:09 -0300

    0.1.8

--- src/components/LGPD.jsx
+++ src/components/LGPD.jsx
@@ -41,9 +41,9 @@ export default function LGPD() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TCZFP3K');` )
+            })(window,document,'script','dataLayer','GTM-MNWTGMJ');` )
 
-            setNoscript(`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCZFP3K"
+            setNoscript(`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNWTGMJ"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`)
         }
        
